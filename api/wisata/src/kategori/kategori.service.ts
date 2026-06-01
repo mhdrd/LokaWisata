07d67da233244fdbb91dd58b7abc8000673dc356
mhdrd <muhammadraditia46@gmail.com>
2026-06-02 00:02:34 +0700

    menambahkan method findOne pada kategori service

--- api/wisata/src/kategori/kategori.service.ts
+++ api/wisata/src/kategori/kategori.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 
@@ -14,5 +14,16 @@ export class KategoriService {
     return this.prisma.kategori.findMany({
       include: { _count: { select: { wisatas: true } } },
     });
+  }
+
+  async findOne(id: number) {
+    const kategori = await this.prisma.kategori.findUnique({
+      where: { id },
+      include: { wisatas: true },
+    });
+    if (!kategori) {
+      throw new NotFoundException(`Kategori dengan id ${id} tidak ditemukan`);
+    }
+    return kategori;
   }
 }
