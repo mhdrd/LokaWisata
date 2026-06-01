8c90f73da15d7bf4abf5d764341e076606fb9de9
mhdrd <muhammadraditia46@gmail.com>
2026-06-01 22:14:59 +0700

    menerapkan internal guard pada wisata controller

--- api/wisata/src/wisata/wisata.controller.ts
+++ api/wisata/src/wisata/wisata.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { WisataService } from './wisata.service';
 import { CreateWisataDto } from './dto/create-wisata.dto';
 import { UpdateWisataDto } from './dto/update-wisata.dto';
+import { InternalGuard } from '../internal.guard';
 
 @Controller('wisata')
+@UseGuards(InternalGuard)
 export class WisataController {
   constructor(private readonly wisataService: WisataService) {}
 
