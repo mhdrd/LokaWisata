03e222282c055da6bf0cfd134709b06093cc793f
mhdrd <muhammadraditia46@gmail.com>
2026-06-01 21:24:25 +0700

    mengimport prisma service pada module wisata

--- api/wisata/src/wisata/wisata.module.ts
+++ api/wisata/src/wisata/wisata.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WisataService } from './wisata.service';
 import { WisataController } from './wisata.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [WisataController],
-  providers: [WisataService],
+  providers: [WisataService, PrismaService],
 })
 export class WisataModule {}
