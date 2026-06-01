ed898888de58a83ea606dfbb6ec051a7fd7ef29f
mhdrd <muhammadraditia46@gmail.com>
2026-06-01 21:24:06 +0700

    mendaftarkan prisma service sebagai provider global

--- api/wisata/src/app.module.ts
+++ api/wisata/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 import { WisataModule } from './wisata/wisata.module';
 
 @Module({
   imports: [WisataModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
