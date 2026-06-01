7a8c76afb3ad95eeae24fb63d2a0af0297283052
mhdrd <muhammadraditia46@gmail.com>
2026-06-01 20:04:22 +0700

    dto

--- api/wisata/src/wisata/dto/create-wisata.dto.ts
+++ api/wisata/src/wisata/dto/create-wisata.dto.ts
@@ -1,1 +1,9 @@
-export class CreateWisataDto {}
+export class CreateWisataDto {
+  name: string;
+  description?: string;
+  contactWa?: string;
+  contactEmail?: string;
+  latitude?: number;
+  longitude?: number;
+  kategoriId: number;
+}
