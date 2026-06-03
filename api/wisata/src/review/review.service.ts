97164cdf7ff59c80f3f895d176f9b7b29e2ec906
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 16:17:40 +0700

    membuat review service dengan method create

---
+++ api/wisata/src/review/review.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+
+@Injectable()
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createReviewDto: CreateReviewDto) {
+    return this.prisma.review.create({ data: createReviewDto });
+  }
+}
