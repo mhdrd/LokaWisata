c378c9d3866bfe9cd45b59340da049e2e88e8768
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 16:15:09 +0700

    mendaftarkan kategori module di app module

--- api/wisata/src/app.module.ts
+++ api/wisata/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { WisataModule } from './wisata/wisata.module';
+import { KategoriModule } from './kategori/kategori.module';
 
 @Module({
-  imports: [WisataModule],
+  imports: [WisataModule, KategoriModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
