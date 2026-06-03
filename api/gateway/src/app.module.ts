8fba7b006d1de47083d3eef80a4c8f75f92cfd03
mhdrd <muhammadraditia46@gmail.com>
2026-06-04 00:45:05 +0700

    mendaftarkan wisata proxy module di app module gateway

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { WisataProxyModule } from './wisata-proxy/wisata-proxy.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, WisataProxyModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
