af172a1afd3ef73282abc0dff62839131351f09b
mhdrd <muhammadraditia46@gmail.com>
2026-06-04 01:09:01 +0700

    mendaftarkan semua proxy module di app module gateway

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { WisataProxyModule } from './wisata-proxy/wisata-proxy.module';
+import { KategoriProxyModule } from './kategori-proxy/kategori-proxy.module';
+import { ReviewProxyModule } from './review-proxy/review-proxy.module';
+import { FavoriteProxyModule } from './favorite-proxy/favorite-proxy.module';
+import { ImageProxyModule } from './image-proxy/image-proxy.module';
 
 @Module({
-  imports: [AuthModule, WisataProxyModule],
+  imports: [
+    AuthModule,
+    WisataProxyModule,
+    KategoriProxyModule,
+    ReviewProxyModule,
+    FavoriteProxyModule,
+    ImageProxyModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
