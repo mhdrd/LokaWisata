c994e5c35770519901592a41e224756648c3313e
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 23:11:47 +0700

    mendaftarkan auth module di app module gateway

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
