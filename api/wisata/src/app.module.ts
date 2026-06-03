d09619757c29b26db7013f09911191f75015653f
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 17:22:39 +0700

    mendaftarkan image dan favorite module di app module

--- api/wisata/src/app.module.ts
+++ api/wisata/src/app.module.ts
@@ -5,9 +5,11 @@ import { PrismaService } from './prisma.service';
 import { WisataModule } from './wisata/wisata.module';
 import { KategoriModule } from './kategori/kategori.module';
 import { ReviewModule } from './review/review.module';
+import { WisataImageModule } from './wisata-image/wisata-image.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [WisataModule, KategoriModule, ReviewModule],
+  imports: [WisataModule, KategoriModule, ReviewModule, WisataImageModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
