fcf7dd97fdf777076cb5ad661e3510e5235d1ecf
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 20:29:15 +0700

    membuat auth service dengan method register

---
+++ api/gateway/src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: registerDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email sudah terdaftar');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        ...registerDto,
+        password: hashedPassword,
+      },
+    });
+
+    const { password, ...result } = user;
+    return result;
+  }
+}
