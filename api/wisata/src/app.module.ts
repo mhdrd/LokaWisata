b0364cef7da24fa9c050c2e85c1450a28a2ef755
mhdrd <muhammadraditia46@gmail.com>
2026-06-03 16:31:02 +0700

    mendaftarkan review module di app module

--- api/wisata/src/app.module.ts
+++ api/wisata/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { WisataModule } from './wisata/wisata.module';
 import { KategoriModule } from './kategori/kategori.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [WisataModule, KategoriModule],
+  imports: [WisataModule, KategoriModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
