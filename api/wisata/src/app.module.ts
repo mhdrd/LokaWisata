3a7fa416bfc4aaebb2728710cf068e232a1153a8
mhdrd <muhammadraditia46@gmail.com>
2026-06-04 01:37:40 +0700

    menambahkan static file serving untuk uploads

--- api/wisata/src/app.module.ts
+++ api/wisata/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
@@ -9,7 +11,17 @@ import { WisataImageModule } from './wisata-image/wisata-image.module';
 import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [WisataModule, KategoriModule, ReviewModule, WisataImageModule, FavoriteModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    WisataModule,
+    KategoriModule,
+    ReviewModule,
+    WisataImageModule,
+    FavoriteModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
