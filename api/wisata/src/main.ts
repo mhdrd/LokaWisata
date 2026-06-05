56d59cbbe0eba51be4c9f4ba459ae6411061c4e5
mhdrd <muhammadraditia46@gmail.com>
2026-06-05 19:41:37 +0700

    mendaftarkan response interceptor di main.ts wisata

--- api/wisata/src/main.ts
+++ api/wisata/src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
