16f41edb6451c7d26e09aa72707af55357fd3c38
Kikoharuna <naufallgt@gmail.com>
2026-06-13 23:51:35 +0700

    menambahkan container dashboard

--- frontend-cms/src/app/dashboard/page.tsx
+++ frontend-cms/src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 export default function DashboardPage() {
   return (
-    <div>Dashboard Admin</div>
+    <div className="min-h-screen bg-gray-50 p-8">
+      <div>Dashboard Admin</div>
+    </div>
   );
 }
