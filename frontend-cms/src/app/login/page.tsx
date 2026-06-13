138c253850d94b6fcb118a808093f1163d7ddd4c
Kikoharuna <naufallgt@gmail.com>
2026-06-13 22:58:32 +0700

    menambahkan judul login

--- frontend-cms/src/app/login/page.tsx
+++ frontend-cms/src/app/login/page.tsx
@@ -2,7 +2,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-xl">
-        Login
+        <h1 className="text-3xl font-bold text-center text-gray-900">LokaWisata</h1>
       </div>
     </div>
   );
