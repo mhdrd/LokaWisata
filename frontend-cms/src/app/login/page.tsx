750bfb748fce6738264628bd8f4c7041389a7459
Kikoharuna <naufallgt@gmail.com>
2026-06-13 22:58:08 +0700

    menambahkan container halaman login

--- frontend-cms/src/app/login/page.tsx
+++ frontend-cms/src/app/login/page.tsx
@@ -1,7 +1,9 @@
 export default function LoginPage() {
   return (
-    <div>
-      Login
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-xl">
+        Login
+      </div>
     </div>
   );
 }
